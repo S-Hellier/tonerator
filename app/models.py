fcb31a5bcec0f94d145524b1a4271d5e4c3f42cd
S-Hellier <stopphellier@gmail.com>
2025-08-03 12:03:09 -0400

    Guitar model

--- app/models.py
+++ app/models.py
@@ -14,3 +14,16 @@ class User(Base):
     amps = Column( JSON, nullable=False, default=dict )
     pedals = Column( JSON, nullable=False, default=dict )
     current_rig = Column( JSON, nullable=False, default=dict )
+
+class Guitar(Base):
+    __tablename__ = "guitars"
+    id = Column( UUID(as_uuid=True), primary_key=True, default=uuid.uuid4 )
+    user_id = Column( UUID(as_uuid=True), ForeignKey('users.id'), nullable=False )
+    name = Column( String, nullable=False )
+    brand = Column( String, nullable=False )
+    model = Column( String, nullable=False )
+    year = Column( String, nullable=True )
+    type = Column( String, nullable=False )  # e.g., electric, acoustic
+    pickup_config = Column ( JSON, nullable=False ) #SSS, HSS, SS, HH, etc.
+    specs = Column( JSON, nullable=True )  # e.g., pickups, wood type
+    created_at = Column( TIMESTAMP, nullable=False )

e19b31cc12bcdb59a224b7ca9f3e0bb9270a4ed5
Scott Hellier <stopphellier@gmail.com>
2025-08-07 10:51:15 -0400

    Express boilerplate

---
+++ ts-app/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const pool = require('./db')
+const port = 1337
+
+const app = express()
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.post('/', (req, res) => {
+    const { name, location } = req.body
+    res.status(200).send({
+        message: `YOUR KEYS WERE ${name}, ${location}`
+    })
+})
+
+// app.get('/setup', async (req, res) => {
+//     try {
+//         await pool.query('CREATE TABLE users( id ')
+//     } catch (err) {
+//         console.log(err)
+//         res.sendStatus(500)
+//     }
+// })
+
+
+app.listen(port, () => console.log(`Server has started on port ${port}`))
